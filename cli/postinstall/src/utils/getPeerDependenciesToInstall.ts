ba2aab8a2545699d674cfefeefb8bd6165f05777
Dylan Aubrey <dylan.aubrey@bjss.com>
2023-10-16 00:52:28 +0100

    feat: use library to load package json from npm reg

--- cli/postinstall/src/utils/getPeerDependenciesToInstall.ts
+++ cli/postinstall/src/utils/getPeerDependenciesToInstall.ts
@@ -1,12 +1,14 @@
 import { verboseLog } from '@repodog/cli-utils';
+import getPackageJsonFromNpmRegistry from 'package-json';
 import { type PackageJson } from 'type-fest';
 
 export const getPeerDependenciesToInstall = async (name: string) => {
   const nameAndVersion: [string, string][] = [];
 
   try {
-    verboseLog(`Getting ${name} peerDependency names`);
-    const packageJson = (await import(`${name}/package.json`)) as PackageJson;
+    verboseLog(`Getting ${name} packageJson from npm registry`);
+    const packageJson = (await getPackageJsonFromNpmRegistry(name)) as PackageJson;
+    verboseLog(`${name} packageJson:\n${JSON.stringify(packageJson, undefined, 2)}`);
 
     if (!packageJson.peerDependencies) {
       verboseLog(`${name} peerDependencies: 'none'`);
@@ -23,7 +25,18 @@ export const getPeerDependenciesToInstall = async (name: string) => {
 
     verboseLog(`${name} peerDependencies: ${nameAndVersion.join(', ')}`);
     return nameAndVersion;
-  } catch {
+  } catch (error: unknown) {
+    verboseLog(`Error getting ${name} peerDependencies`);
+
+    if (error instanceof Error) {
+      verboseLog(`Error name: ${error.name}`);
+      verboseLog(`Error message: ${error.message}`);
+
+      if (error.stack) {
+        verboseLog(`Error stack: ${error.stack}`);
+      }
+    }
+
     return nameAndVersion;
   }
 };
