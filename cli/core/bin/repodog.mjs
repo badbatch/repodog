125f9af36555cb2f5d550071714dff1cae0dab10
Dylan Aubrey <dylan.aubrey@bjss.com>
2023-11-17 17:17:17 +0000

    fix: change path to dist output

--- cli/core/bin/repodog.mjs
+++ cli/core/bin/repodog.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import 'suppress-experimental-warnings';
 
-const { init } = await import('../dist/main/index.mjs'); // eslint-disable-line import/no-unresolved
+const { init } = await import('../dist/esm/index.mjs'); // eslint-disable-line import/no-unresolved
 init();
