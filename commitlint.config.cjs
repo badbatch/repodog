19cc13277da18bfa78ce66c37b7ad640d1508b38
Dylan Aubrey <dylan.aubrey@bjss.com>
2024-02-23 19:00:56 +0000

    feat(commitlint-config): dynamically generate and enforce scopes

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,17 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const dirents = [
+  ...fs.readdirSync(path.resolve(__dirname, './cli'), { withFileTypes: true }),
+  ...fs.readdirSync(path.resolve(__dirname, './configs'), { withFileTypes: true }),
+];
+
+const directories = dirents.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
+
 module.exports = {
   extends: ['@repodog/commitlint-config'],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'scope-enum': [2, 'always', directories],
+  },
 };
