b1e6658f0f47e7cde8daea69d715c350b64aa8dc
Dylan Aubrey <dylan.aubrey@bjss.com>
2024-02-27 14:37:38 +0000

    feat(cut): add logging to changelog normalisation code

--- cli/cut/src/utils/normaliseChangelog.ts
+++ cli/cut/src/utils/normaliseChangelog.ts
@@ -1,4 +1,4 @@
-import { asyncExec } from '@repodog/cli-utils';
+import { asyncExec, verboseLog } from '@repodog/cli-utils';
 import { existsSync, readFileSync, writeFileSync } from 'node:fs';
 import { resolve } from 'node:path';
 
@@ -6,6 +6,7 @@ export const normaliseChangelog = async (dependencies: Partial<Record<string, st
   const cwd = process.cwd();
 
   if ('markdownlint-cli2' in dependencies && existsSync(resolve(cwd, '.markdownlint.json'))) {
+    verboseLog('markdownlint dependency and config found, normalising Changelog file');
     await asyncExec(`markdownlint-cli2 ./CHANGELOG.md --config .markdownlint.json --fix`);
     let changelog = readFileSync(resolve(cwd, 'CHANGELOG.md'), { encoding: 'utf8' });
 
@@ -16,5 +17,7 @@ export const normaliseChangelog = async (dependencies: Partial<Record<string, st
 
     changelog = `# Changelog\n\n${changelog}`;
     writeFileSync(resolve(cwd, 'CHANGELOG.md'), changelog, { encoding: 'utf8' });
+  } else {
+    verboseLog('No markdownlint dependency or config found, skipping normalising Changelog file');
   }
 };
