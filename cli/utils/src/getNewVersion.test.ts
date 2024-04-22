17fb72f5a16c06451629a791d33ead39af533425
Dylan Aubrey <dylan.aubrey@bjss.com>
2024-04-22 23:10:59 +0100

    fix(utils): bug in prerelease digit incrementing

--- cli/utils/src/getNewVersion.test.ts
+++ cli/utils/src/getNewVersion.test.ts
@@ -4,20 +4,23 @@ describe('getNewVersion', () => {
   describe('when there is a tag', () => {
     it('should return the correct new version', async () => {
       const { getNewVersion } = await import('./getNewVersion.ts');
-      expect(getNewVersion('1.0.0', 'premajor', ReleaseTag.ALPHA)).toBe('2.0.0-alpha');
+      expect(getNewVersion('1.0.0', 'premajor', ReleaseTag.PULL_REQUEST)).toBe('2.0.0-pr.0');
     });
 
     describe('when type is "prerelease"', () => {
       it('should return the correct new version', async () => {
         const { getNewVersion } = await import('./getNewVersion.ts');
-        expect(getNewVersion('2.0.0-alpha.1', 'prerelease')).toBe('2.0.0-alpha.2');
+        expect(getNewVersion('2.0.0-pr.4', 'prerelease', ReleaseTag.PULL_REQUEST)).toBe('2.0.0-pr.5');
       });
     });
 
     describe('when there is an identifier', () => {
       it('should return the correct new version', async () => {
         const { getNewVersion } = await import('./getNewVersion.ts');
-        expect(getNewVersion('2.0.0-alpha', 'prepatch', ReleaseTag.ALPHA, '1234567')).toBe('2.0.1-alpha-1234567');
+
+        expect(getNewVersion('2.0.1-pr-12345.0', 'prerelease', ReleaseTag.PULL_REQUEST, '12345')).toBe(
+          '2.0.1-pr-12345.1'
+        );
       });
     });
   });
--- cli/utils/src/getNewVersion.ts
+++ cli/utils/src/getNewVersion.ts
@@ -2,5 +2,5 @@ import semver, { type ReleaseType } from 'semver';
 import { type ReleaseTag } from './types.ts';
 
 export const getNewVersion = (version: string, type: ReleaseType, tag?: ReleaseTag, preid?: string) => {
-  return semver.inc(version, type, tag && preid ? `${tag}-${preid}` : tag, false);
+  return semver.inc(version, type, tag && preid ? `${tag}-${preid}` : tag, '0');
 };
