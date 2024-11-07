5fa4afecdc67b060ecfa1dea55deeae034d6bb4e
Dylan Aubrey <dylan.aubrey@bjss.com>
2024-11-07 23:12:52 +0000

    refactor(eslint-config-jasmine): update config to get working

--- configs/eslint-config-jasmine/index.mjs
+++ configs/eslint-config-jasmine/index.mjs
@@ -3,9 +3,12 @@ import jasmine from 'eslint-plugin-jasmine';
 // eslint convention is to export default
 // eslint-disable-next-line import-x/no-default-export
 export default [
-  jasmine.configs.recommended,
   {
+    plugins: {
+      jasmine,
+    },
     rules: {
+      ...jasmine.configs.recommended,
       'jasmine/new-line-before-expect': 0,
       'jasmine/no-spec-dupes': 0,
       'jasmine/no-suite-dupes': 0,
