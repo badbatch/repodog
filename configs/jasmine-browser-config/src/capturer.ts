bc746c4d09fb10361a6064f7bb16e8b7bdb275de
miami-man <badbatchdev@gmail.com>
2026-03-13 21:14:21 +0000

    chore(jasmine-browser-config): add logging to server

--- configs/jasmine-browser-config/src/capturer.ts
+++ configs/jasmine-browser-config/src/capturer.ts
@@ -3,17 +3,22 @@ import http from 'node:http';
 import path from 'node:path';
 
 const dir = path.resolve(process.cwd(), 'coverage');
+console.log(`> coverage directory: ${dir}`);
 fs.mkdirSync(dir, { recursive: true });
+console.log(`> created coverage directory`);
 
 http
   .createServer((req, res) => {
     if (req.method === 'POST' && req.url === '/__coverage__') {
+      console.log(`> received coverage data request`);
       let body = '';
 
       req.on('data', (chunk: string) => (body += chunk));
 
       req.on('end', () => {
+        console.log(`> finished coverage data request`);
         const file = path.join(dir, 'jasmine-browser-coverage.json');
+        console.log(`> writing data to: ${file}`);
         fs.writeFileSync(file, body);
         res.end('ok');
       });
@@ -23,4 +28,6 @@ http
 
     res.end();
   })
-  .listen(3002);
+  .listen(3002, () => {
+    console.log(`> listen to port 3002`);
+  });
