6a6ac053cb195aabfabc5d3df472cb156b09482a
miami-man <badbatchdev@gmail.com>
2026-07-03 08:38:29 +0100

    fix(rollup-config): pin version of macros dep

--- .syncpackrc.mjs
+++ .syncpackrc.mjs
@@ -1,7 +1,16 @@
 import config from '@repodog/syncpack-config';
 
+const { versionGroups, ...restConfig } = config;
+
 // Required for SyncPack
 // eslint-disable-next-line import-x/no-default-export
 export default {
-  ...config,
+  versionGroups: [
+    {
+      dependencies: ['unplugin-macros'],
+      pinVersion: '0.20.0',
+    },
+    ...versionGroups,
+  ],
+  ...restConfig,
 };
